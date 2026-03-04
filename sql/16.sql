60df4d3549e74d2f7593679fa08ee28edc571eb8
0marJim <omar.jimenez.us@gmail.com>
2026-03-04 00:15:52 -0800

    tried fixing

--- sql/16.sql
+++ sql/16.sql
@@ -9,4 +9,4 @@ JOIN inventory ON film.film_id = inventory.film_id
 JOIN rental ON inventory.inventory_id = rental.inventory_id
 JOIN payment ON rental.rental_id = payment.rental_id
 GROUP BY film.film_id, film.title
-ORDER BY profit DESC;
+ORDER BY profit DESC, film.title ASC;
--- sql/17.sql
+++ sql/17.sql
@@ -9,6 +9,7 @@ FROM country
 JOIN city ON country.country_id = city.country_id
 JOIN address ON city.city_id = address.city_id
 JOIN customer ON address.address_id = customer.address_id
-JOIN payment ON customer.customer_id = payment.customer_id
+JOIN rental ON customer.customer_id = rental.customer_id
+JOIN payment ON rental.rental_id = payment.customer_id
 GROUP BY country.country
 ORDER BY country.country ASC;
